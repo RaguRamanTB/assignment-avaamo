1f83c08577a82fe607e03a44dec2a838b8d928c3
Raguraman T B <aakash1956ragu@gmail.com>
2024-04-05 01:47:47 +0530

    Finding meanings of the words

--- file-data-analyzer-ui/src/components/DataTable.jsx
+++ file-data-analyzer-ui/src/components/DataTable.jsx
@@ -1,29 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { Table } from "antd";
+import { Skeleton, Table } from "antd";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
-
-const columns = [
-  {
-    title: "Word",
-    dataIndex: "word",
-    width: "30%",
-  },
-  {
-    title: "Count",
-    dataIndex: "count",
-    width: "20%",
-  },
-  {
-    title: "Meaning",
-    dataIndex: "meaning",
-    width: "50%",
-  },
-];
+import axios from "axios";
+import { dictionaryApi } from "../utils/apiRoutes";
 
 const DataTable = ({ selectedFile }) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
   const analysisData = useSelector((state) => state.analytics.analysisData);
+
+  const columns = [
+    {
+      title: "Word",
+      dataIndex: "word",
+      width: "30%",
+    },
+    {
+      title: "Count",
+      dataIndex: "count",
+      width: "20%",
+    },
+    {
+      title: "Synonym",
+      dataIndex: "synonym",
+      width: "50%",
+      render: (text) =>
+        loading ? (
+          <Skeleton.Input active size="small" />
+        ) : (
+          <p
+            style={{
+              margin: 0,
+              padding: 0,
+              fontWeight: text === "No synonym found" ? 700 : 0,
+            }}
+          >
+            {text}
+          </p>
+        ),
+    },
+  ];
 
   useEffect(() => {
     if (!selectedFile) {
@@ -39,11 +56,45 @@ const DataTable = ({ selectedFile }) => {
         key: word,
         word,
         count: fileData[word],
-        meaning: "Meaning",
+        synonym: "synonym",
       };
     });
     setData(tableData);
+    fetchMeanings(tableData.slice(0, 10), 0);
   }, [selectedFile, analysisData]);
+
+  const fetchMeanings = async (currentData, start) => {
+    setLoading(true);
+    const updatedData = await Promise.all(
+      currentData.map(async (row) => {
+        const response = await axios.get(dictionaryApi + row.word);
+        if (response.status !== 200) {
+          return { ...row, synonym: "Failed to fetch synonym" };
+        }
+        const { def } = response.data;
+        if (!def.length) {
+          return { ...row, synonym: "No synonym found" };
+        }
+        return { ...row, synonym: def[0].tr[0].text };
+      })
+    );
+    setData((prevData) => {
+      const newData = [...prevData];
+      for (let i = 0; i < updatedData.length; i++) {
+        newData[start + i] = updatedData[i];
+      }
+      return newData;
+    });
+    setLoading(false);
+  };
+
+  const handleTableChange = (pagination) => {
+    const { current, pageSize } = pagination;
+    const start = (current - 1) * pageSize;
+    const end = start + pageSize;
+    const currentPageData = data.slice(start, end);
+    fetchMeanings(currentPageData, start);
+  };
 
   return (
     <Container>
@@ -54,6 +105,7 @@ const DataTable = ({ selectedFile }) => {
         scroll={{
           y: 360,
         }}
+        onChange={handleTableChange}
       />
     </Container>
   );
--- file-data-analyzer-ui/src/utils/apiRoutes.js
+++ file-data-analyzer-ui/src/utils/apiRoutes.js
@@ -3,3 +3,6 @@ export const apiRoutes = {
   upload: `${host}/api/files/upload`,
   getAnalysis: `${host}/api/data/getAnalysis`,
 };
+const APIkey =
+  "dict.1.1.20170610T055246Z.0f11bdc42e7b693a.eefbde961e10106a4efa7d852287caa49ecc68cf";
+export const dictionaryApi = `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${APIkey}&lang=en-en&text=`;
