9a1ccc4e297b5486be7a1a171c64091e98f996f6
Raguraman T B <aakash1956ragu@gmail.com>
2024-04-04 20:22:12 +0530

    Setting up MongoDB

--- file-data-analyzer-api/index.js
+++ file-data-analyzer-api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const fileRoutes = require("./routes/fileRoutes");
 
@@ -10,6 +11,20 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/files", fileRoutes);
+
+// Connect to MongoDB
+const uri = process.env.MONGODB_URI;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port: ${process.env.PORT || 5000}`);
