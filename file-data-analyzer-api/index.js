72192a67b51804069726c9b64ecda942f1242158
Raguraman T B <aakash1956ragu@gmail.com>
2024-04-04 12:00:01 +0530

    Writing upload route for File Upload using Multer

--- file-data-analyzer-api/index.js
+++ file-data-analyzer-api/index.js
@@ -1,5 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+require("dotenv").config();
+const upload = require("./middlewares/multerUpload");
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  if (req.file) {
+    res.status(201).json({
+      message: "File uploaded successfully",
+      file: req.file,
+    });
+  } else {
+    res.status(400).json({
+      message: "File not uploaded",
+    });
+  }
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port: ${process.env.PORT || 5000}`);
---
+++ file-data-analyzer-api/middlewares/multerUpload.js
@@ -0,0 +1,16 @@
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    fs.mkdirSync(path.join(__dirname, "../uploads"), { recursive: true });
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+module.exports = upload;
