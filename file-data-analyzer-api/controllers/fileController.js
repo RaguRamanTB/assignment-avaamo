282cf85585cf6a8612b81b9f86e79c5edb9bb114
Raguraman T B <aakash1956ragu@gmail.com>
2024-04-04 20:38:37 +0530

    Added File model and data creation on upload

--- file-data-analyzer-api/controllers/fileController.js
+++ file-data-analyzer-api/controllers/fileController.js
@@ -1,9 +1,18 @@
+const File = require("../models/fileModel");
+
 module.exports.uploadFile = async (req, res, next) => {
   try {
     if (req.file) {
+      const file = await File.create({
+        originalname: req.file.originalname,
+        filename: req.file.filename,
+        size: req.file.size,
+        type: req.file.mimetype,
+        path: req.file.path,
+      });
       res.status(201).json({
         message: "File uploaded successfully",
-        file: req.file,
+        file,
       });
     } else {
       res.status(400).json({
---
+++ file-data-analyzer-api/models/fileModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const fileSchema = new mongoose.Schema(
+  {
+    originalname: {
+      type: String,
+      required: true,
+    },
+    filename: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    size: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+    wordCount: {
+      type: Number,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("File", fileSchema);
