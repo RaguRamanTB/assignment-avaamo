7fc733a82a0f46f482df8394edd2ad4ac24f093f
Raguraman T B <aakash1956ragu@gmail.com>
2024-04-05 23:07:52 +0530

    Added mask route and changed Yandex Key

--- file-data-analyzer-ui/src/utils/apiRoutes.js
+++ file-data-analyzer-ui/src/utils/apiRoutes.js
@@ -2,7 +2,8 @@ export const host = "http://localhost:5000";
 export const apiRoutes = {
   upload: `${host}/api/files/upload`,
   getAnalysis: `${host}/api/data/getAnalysis`,
+  mask: `${host}/api/data/mask`,
 };
 const APIkey =
-  "dict.1.1.20170610T055246Z.0f11bdc42e7b693a.eefbde961e10106a4efa7d852287caa49ecc68cf";
+  "dict.1.1.20240405T173622Z.56e88fde1c34475c.1f9cd19ec0c4611f5af227da1cff50d1b448438f";
 export const dictionaryApi = `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${APIkey}&lang=en-en&text=`;
